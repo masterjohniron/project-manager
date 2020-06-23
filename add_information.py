8401bab23b1f5670d029978bd5f80b7e9ff03439
masterjohniron <67309552+masterjohniron@users.noreply.github.com>
2020-06-22 23:55:17 -0700

    Add files via upload

---
+++ add_information.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import os
+
+file_name = 'server_delopy.csv'
+date = [pd.to_datetime('2020-06-23')]
+hour = [11]
+location = ['2901 Coronado Dr']
+number = [31]
+company = ['alibaba']
+
+result = pd.DataFrame(columns=('date', 'hour', 'location', 'number', 'company'))
+data = {'date': date, 'hour': hour, 'location': location, 'number': number, 'company': company}
+result = result.append(pd.DataFrame(data), ignore_index=True)
+print(result)
+if os.path.exists(file_name):
+    result.to_csv(file_name, index=None, mode='a', header=False)  # 保存文档，如果已存在，则不保存列名
+else:
+    result.to_csv(file_name, index=None, mode='a')
